b17936fa957f389e6d3482cd9840b96f9ee0f798
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2026-06-24 14:59:47 +0200

    import QC flags to main __inin__

--- tests/test_flags.py
+++ tests/test_flags.py
@@ -1,44 +1,35 @@
 from __future__ import annotations
 
-from marine_qc.helpers.auxiliary import (
-    best,
-    duplicate,
-    failed,
-    passed,
-    unique,
-    untestable,
-    untested,
-    worst,
-)
+from marine_qc import Flags
 
 
 def test_passed_flag():
-    assert passed == 0
+    assert Flags.passed == 0
 
 
 def test_failed_flag():
-    assert failed == 1
+    assert Flags.failed == 1
 
 
 def test_untestable_flag():
-    assert untestable == 2
+    assert Flags.untestable == 2
 
 
 def test_untested_flag():
-    assert untested == 3
+    assert Flags.untested == 3
 
 
 def test_unique_flag():
-    assert unique == 0
+    assert Flags.unique == 0
 
 
 def test_best_flag():
-    assert best == 1
+    assert Flags.best == 1
 
 
 def test_any_flag():
-    assert duplicate == 2
+    assert Flags.duplicate == 2
 
 
 def test_worst_flag():
-    assert worst == 3
+    assert Flags.worst == 3
