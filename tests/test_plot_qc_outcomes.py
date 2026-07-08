01953e5848c4427679c4b3df23cd56bb5a8ffecb
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2026-07-08 09:06:35 +0200

    split plo_latitude_longitude plots

--- tests/test_plot_qc_outcomes.py
+++ tests/test_plot_qc_outcomes.py
@@ -64,6 +64,13 @@ def test_plot_latitude_longitude():
         lat=np.array([-10, 0, 10]),
         lon=np.array([-10, 0, 10]),
         qc_outcomes=np.array([0, 1, 2]),
+    )
+    plt.close(fig)
+    
+    fig = plot_latitude_longitude(
+        lat=np.array([-10, 0, 10]),
+        lon=np.array([-10, 0, 10]),
+        qc_outcomes=np.array([0, 1, 2]),
         add_coastlines=True,
         marker_size=2,
         xlim=(-15, 15),
