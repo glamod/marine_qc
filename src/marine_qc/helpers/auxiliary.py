5f16907eb4c4d9c738b7df8bf7b2e31d8ff36cf1
Ludwig Lierhammer <Ludwig.Lierhammer@dwd.de>
2026-07-29 10:26:26 +0200

    Update src/marine_qc/helpers/auxiliary.py

--- src/marine_qc/helpers/auxiliary.py
+++ src/marine_qc/helpers/auxiliary.py
@@ -817,16 +817,6 @@ def args_from_data(*params: str) -> Callable[..., Any]:
                 If both positional arguments and the ``data`` keyword
                 argument are provided.
             """
-
-            def isin(param, data):
-                if isinstance(data, pd.DataFrame):
-                    return param in data.columns
-                elif isinstance(data, xr.DataArray):
-                    return param in data.coords or param in data.dims
-                elif isinstance(data, xr.Dataset):
-                    return param in data.data_vars or param in data.coords or param in data.dims
-                raise TypeError(f"Type of 'data' must be one of pd.DataFrame, xr.DataArray or xr.Dataset not {type(data)}.")
-
             if "data" not in kwargs:
                 return func(*args, **kwargs)
             if len(args) > 0:
